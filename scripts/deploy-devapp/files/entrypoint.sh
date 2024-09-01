bd28e186df10bdfad1123c12603fb51becdd65a5
egoavara <egoavara@gmail.com>
2024-09-01 20:19:22 +0900

    chore: watchexec 변경

--- scripts/deploy-devapp/files/entrypoint.sh
+++ scripts/deploy-devapp/files/entrypoint.sh
@@ -2,4 +2,4 @@ apk add watchexec
 
 sleep 1s
 
-watchexec -w /git/cokane-authz -r "cd /git/cokane-authz && go run ./main.go manage"
+watchexec -w /git/cokane-authz -r -- "cd /git/cokane-authz; go run ./main.go manage"
